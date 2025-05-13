eb4b3d8e192f03ed994fabaead320bef17909b58
Darus Salam <darussalam66d@gmail.com>
2025-05-13 08:02:34 +0600

    update

--- lib/code_list/if_statement.dart
+++ lib/code_list/if_statement.dart
@@ -5,7 +5,7 @@ void main(){
   int age = 36;
 
 
-  print("Example 1: simple if statement");
+  print("\nExample 1: simple if statement");
 
   if(age>=18){
     print("You are an adult");
@@ -38,7 +38,25 @@ void main(){
     print("you are a minor");
   }
 
-  // Example 4
+  // Example 4 basic else if ladder with numbers and conditions
+
+  int score = 85;
+
+  print("\nExample 4: basic else if ladder with numbers");
+
+  if (score>=91 && score <=100){
+    print("Grade is: A+");
+  }else if (score >=81 && score <=90){
+    print("Grade is: A");
+  }else if (score >=71 && score >=80){
+    print("Grade is: B");
+  }else if (score >=61 && score >=70){
+    print("Grade is: c");
+  }else if (score >=51 && score >=60){
+    print("Grade is: D");
+  }else {
+    print("Grade is: F");
+  }
 
 
 
