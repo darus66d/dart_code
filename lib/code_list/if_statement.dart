d676fddb37ee550de0cedf925218641403b5b048
Darus Salam <darussalam66d@gmail.com>
2025-05-13 06:56:05 +0600

    update

---
+++ lib/code_list/if_statement.dart
@@ -0,0 +1,16 @@
+void main(){
+  //control flow / conditions
+
+  // Example 1  simple if statement
+  int age = 20;
+
+
+  print("Example 1: simple if statement");
+
+  if(age>=18){
+    print("Your are an adult");
+  }
+
+
+
+}
