b5c5799ae6b01c27655933a01d5c0671ec232907
Darus Salam <darussalam66d@gmail.com>
2025-05-25 09:41:32 +0600

    update

--- lib/code_list/else_if_ladder.dart
+++ lib/code_list/else_if_ladder.dart
@@ -6,16 +6,16 @@ void main() {
   print("\nExample 1: basic else if ladder with numbers");
 
   if (score >= 91 && score <= 100) {
-    print("Grade is: A+");
+    print(" Your Grade is: A+");
   } else if (score >= 81 && score <= 90) {
-    print("Grade is: A");
+    print(" Your Grade is: A");
   } else if (score >= 71 && score >= 80) {
-    print("Grade is: B");
+    print(" Your Grade is: B");
   } else if (score >= 61 && score >= 70) {
-    print("Grade is: c");
+    print("Your Grade is: c");
   } else if (score >= 51 && score >= 60) {
-    print("Grade is: D");
+    print("Your Grade is: D");
   } else {
-    print("Grade is: F");
+    print(" Your Grade is: F");
   }
 }
--- lib/code_list/nested_if_else_statements.dart
+++ lib/code_list/nested_if_else_statements.dart
@@ -1,4 +1,79 @@
 void main(){
-  int age = 20;
 
+  //Example 1: basic nested If else
+
+  int age = 20;
+  bool hasId = true;
+  //bool hasId = false;
+
+  print("Example 1: basic Nested if else\n");
+  if(age>=18){
+    if(hasId){
+      print("You can enter the venue");
+    }else{
+      print("You need to show your id ");
+    }
+  }else{
+    print("Your are too young to get into the venue");
+  }
+
+  //Example 2: nested if else with multiple conditions
+
+  int temparature = 25;
+  bool isRaining = true;
+  //  bool hasUmbrella = true;
+  bool hasUmbrella = false;
+
+  print("\nExample 2: Nested if else with multiple conditions\n");
+  
+  if(temparature>=20){
+    if(isRaining){
+      if(hasUmbrella){
+        print("You can go outside");
+      }else{
+        print("You should stay inside or bring with you an umbrella");
+      }
+    }else {
+      print("It's a nice day you can go outside");
+    }
+  }else{
+    print("It's cold to go outside");
+  }
+
+  //Example 3: nested if-else with number ranges
+
+  int score = 85;
+  bool isPassing = true;
+
+  print("\nExample 3: nested if else with number ranges\n");
+
+  if(score>=70){
+    if(score>=90){
+      print("You pass with grade A");
+    }else if(score>=80){
+      print("You are pass with grade B");
+    }else{
+      print("Your are pass with grade C");
+    }
+  }else {
+    print("You need to retake the exam");
+  }
+
+  //Example 4: Nested if-else with string and number conditions
+   String name = "Darus";
+  int age1 = 26;
+
+  print("\nExample 4: nested if else with string and number condtions");
+
+  if(name=="Darus"){
+    if(age1 >=25){
+      print("Welcome Darus! you can enter the event");
+    }else{
+      print("Sorry Darus! your are too young to get into the event");
+    }
+  }else{
+    print("Sorry! It's a private event for Darus");
+  }
+
+  //Example
 }
