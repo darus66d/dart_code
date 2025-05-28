04a9819e21a37ec2dcf8ab0d0e892e1441331102
Darus Salam <darussalam66d@gmail.com>
2025-05-28 08:51:33 +0600

    update

--- lib/code_list/null_aware_operators.dart
+++ lib/code_list/null_aware_operators.dart
@@ -3,8 +3,22 @@ void main(){
   //Example 1: basic null aware operators
 
   String? name;
+  
+  print("\nExample 1: Basic null aware operators");
 
   String greeting = name ?? "guest";
 
   print("Hello! $greeting");
+  
+  //Example 2: null aware assignment operator
+  
+  String ? message;
+  
+  print("\nExample 2: null aware assignment operator");
+
+  message ??= "Default message";
+  print("Message: $message");
+  
+  
+  
 }
