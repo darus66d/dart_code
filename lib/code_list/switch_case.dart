44584869d5d9d0ccad52381deb45f5a6fc0c5699
Darus Salam <darussalam66d@gmail.com>
2025-05-28 10:26:45 +0600

    update

---
+++ lib/code_list/switch_case.dart
@@ -0,0 +1,32 @@
+void main(){
+
+  //Example 1: basic switch  case
+
+  int day = 3;
+
+  print("\nExample 1: Basic Switch Case");
+
+  switch(day){
+    case 1:
+      print("Sunday");
+      break;
+    case 2:
+      print("Monday");
+      break;
+    case 3:
+      print("Tuesday");
+      break;
+    case 4:
+      print("wednesday");
+      break;
+    case 5:
+      print("Thursday");
+      break;
+    case 6:
+      print("Friday");
+      break;
+    default:
+      print("Saturday");
+
+  }
+}
