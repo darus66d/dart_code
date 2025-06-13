6e8305081d094ea765b08308d00770f0e37443ef
Darus Salam <darussalam66d@gmail.com>
2025-06-13 13:02:15 +0600

    update

---
+++ lib/code_list/loop.dart
@@ -0,0 +1,21 @@
+void main(){
+
+  //Example 1: basic loop
+  print("\nExample 1: Basic loop");
+  //for loop
+  for(int i=1;i<=100;i++){
+    print("count: $i");
+  }
+  print("Program is Ended");
+
+  //Example 2: For in loop
+  print("\nExample 2: For in loop");
+  List<int> numbers = [1,2,3,4,5];
+  for(int number in numbers){
+    print("number: $number");
+  }
+
+
+
+
+}
