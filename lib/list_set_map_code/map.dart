3bc13abae9de5af3d2e1c1d62af82927b0d50851
Darus Salam <darussalam66d@gmail.com>
2025-06-25 10:48:16 +0600

    update

---
+++ lib/list_set_map_code/map.dart
@@ -0,0 +1,18 @@
+void main(){
+
+
+    //key value pair
+
+  //Example 1: basic Map
+
+  print("\nExample 1: basic map");
+
+  Map<String, dynamic> info = {
+    "Darus" : "015854558",
+    "Salam" : "018558482",
+    "Emon" : "016296913",
+  };
+  print("Information: $info");
+
+
+}
