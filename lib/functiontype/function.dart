35d2050570834d16a47b9725452b2b921bc6229e
Darus Salam <darussalam66d@gmail.com>
2025-06-27 09:27:21 +0600

    update

--- lib/functiontype/function.dart
+++ lib/functiontype/function.dart
@@ -1,23 +1,30 @@
 void main(){
+  print("\nExample 1: basic function create");
  addTwo();
+ print("\nExample 2: Regular function create");
  sumTwoNum(10, 20);
  sumTwoNum(30, 40);
  sumTwoNum(3, 50);
+ print("\nExample 3: Create a positional parameter function");
+ person("Darus", 27, "Dhaka");
+
 }
 //Example 1: basic function create
 //write your own function
 addTwo(){
-  print("\nExample 1: basic function create");
   var x= 10;
   var y= 20;
   var z= x+y;
   print("Sum: $z");
 }
-//Example 2:
+//Example 2: create a regular function
 //write your regular function
-
-
 sumTwoNum(a,b){
   var sum = a+b;
   print(sum);
+}
+//Example 3: Create a positional parameter function
+
+person(name,age,city){
+  print("Name is $name age $age City $city");
 }
