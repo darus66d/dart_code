a2a03ca20fa400eed10165e2c096692b5f96bf5b
Darus Salam <darussalam66d@gmail.com>
2025-06-27 08:35:10 +0600

    update

--- lib/functiontype/function.dart
+++ lib/functiontype/function.dart
@@ -1,14 +1,23 @@
 void main(){
-
  addTwo();
-
+ sumTwoNum(10, 20);
+ sumTwoNum(30, 40);
+ sumTwoNum(3, 50);
 }
-
+//Example 1: basic function create
 //write your own function
-
 addTwo(){
+  print("\nExample 1: basic function create");
   var x= 10;
   var y= 20;
   var z= x+y;
   print("Sum: $z");
+}
+//Example 2:
+//write your regular function
+
+
+sumTwoNum(a,b){
+  var sum = a+b;
+  print(sum);
 }
