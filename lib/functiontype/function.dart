46ec306487fccddb70e7c59bb0ceecd3fdc51e5a
Darus Salam <darussalam66d@gmail.com>
2025-06-30 10:33:47 +0600

    update

--- lib/functiontype/function.dart
+++ lib/functiontype/function.dart
@@ -17,6 +17,11 @@ void main(){
  myFun(50,60);
  print("\nExample 6: InLine Function / Arrow Function");
  inLine(40,60);
+ print("\n//Example 7: Function return types");
+ int result = abSquare(2, 3);
+ print("Result: $result");
+ print("\nExample 8: Higher oder function/ call back function");
+ fun2(fun1(10,20));
 }
 //Example 1: Create regular function
 //write your own function
@@ -42,7 +47,7 @@ personInfo({ required String? name, required int age , required String city}){
 }
 
 
-//Example 4 Create a default Function
+//Example 4 Create a default parameter Function
 
 addNumbers({a, b, c, d = 100}){
   print(a+b+c+d);
@@ -58,4 +63,24 @@ var myFun=(x,y){
 
 var inLine= (x,y)=>print(x+y);
 
-//Example 7:
+//Example 7: Function return types
+// কোন ফাংশন যখন কিছু রিটার্ন করে , গুনে ও মানে উহা তাহার
+// সমান হয়ে যায়
+
+int abSquare(a,b){
+  return(a*a)+(2*a*b)+(b*b);
+}
+
+//Example 8: Higher oder function/ call back function
+
+fun1(x,y){
+  print(x+y);
+}
+fun2(callbackFunction){
+  callbackFunction();
+}
+
+bangladesh(dhaka,ctg,rang,kul,raj){
+  var totalBangladesh = dhaka()+ctg()+rang()+kul()+raj();
+  print(totalBangladesh);
+}
