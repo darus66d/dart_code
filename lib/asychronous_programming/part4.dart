14f971ab52c7c70538957755f589dddf1637e5d3
Darus Salam <darussalam66d@gmail.com>
2025-08-21 11:35:02 +0600

    update

---
+++ lib/asychronous_programming/part4.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'dart:io';
+
+main(){
+  callData();
+
+}
+
+Future<void> callData() async{
+  String URL = "https://jsonplaceholder.typicode.com/todos/1";
+  print("Loading..");
+  final request = await HttpClient().getUrl(Uri.parse(URL));
+  request.headers.set(HttpHeaders.acceptHeader, "application/json");
+  final response = await request.close();
+  final responseUtf8 = await response.transform(utf8.decoder).join();
+  final data = jsonDecode(responseUtf8);
+  print(data);
+}
